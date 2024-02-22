866cb909615ab7a59ea3a51d213cdfc7faa9016d
Grigoriy Buranov <buranovg4@gmail.com>
2024-02-22 14:05:32 +0400

    first

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&qn4crt$2siog^5&1n2b!rm%a!i6fuqygr*!n+j8bt*_p1b$!p'
+SECRET_KEY = '9274012741907483'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
